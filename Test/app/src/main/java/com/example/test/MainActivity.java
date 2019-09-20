1f4ab65fa50c16c2a681d77549229f3fd16ff7f9
Markss07 <markinfotech07@gmail.com>
2019-09-20 11:32:19 +0530

    Added whole project 	initial commit 	Added new Fragment with package

---
+++ Test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ToolBar
+     * */
+    public static TextView titleMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        titleMain = (TextView)toolbar.findViewById(R.id.titleMain);
+    }
+}
